f17810cec73300c867085b3257583bde20cb7acc
Guilherme <guilherme.salviano12@outlook.com>
2026-04-11 15:37:49 -0300

    chore: remove @ importation

--- apps/client/src/server.ts
+++ apps/client/src/server.ts
@@ -1,5 +1,5 @@
 import { config } from './config'
-import { app, logger } from '@/app'
+import { app, logger } from './app'
 
 app.listen(config.PORT, () => {
   logger.log('info', `Server running at http://localhost:${config.PORT}`)
